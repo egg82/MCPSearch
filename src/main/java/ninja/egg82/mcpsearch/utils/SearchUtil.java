7f5de3fc4cbefd5b9eddb9b296ead5088c783ab2
egg82 <eggys82@gmail.com>
2019-08-08 20:23:35 -0600

    Updated Levenshtein text

--- src/main/java/ninja/egg82/mcpsearch/utils/SearchUtil.java
+++ src/main/java/ninja/egg82/mcpsearch/utils/SearchUtil.java
@@ -1,10 +1,12 @@
 package ninja.egg82.mcpsearch.utils;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 public class SearchUtil {
+    private static LevenshteinDistance distanceFunction = LevenshteinDistance.getDefaultInstance();
+
     public static int getDistance(String haystack, String needle) {
-        return StringUtils.getLevenshteinDistance(haystack, needle);
+        return distanceFunction.apply(haystack, needle);
     }
 
     public static int getDistanceSubstring(String haystack, String needle) {
@@ -15,7 +17,7 @@ public class SearchUtil {
         int max = haystack.length() - needle.length();
         int distance = Integer.MAX_VALUE;
         for (int i = 0; i < max; i++) {
-            distance = Math.min(distance, StringUtils.getLevenshteinDistance(haystack.substring(i, i + needle.length()), needle));
+            distance = Math.min(distance, distanceFunction.apply(haystack.substring(i, i + needle.length()), needle));
         }
         return distance;
     }
